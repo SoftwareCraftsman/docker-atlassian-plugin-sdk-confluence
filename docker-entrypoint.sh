b8313b83a1bb3cf69af91a5aa3e18c689230bd6d
Martin Greilinger <martin.greilinger@aon.at>
2021-02-10 10:49:04 +0100

    Cleanup entry point script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -x
 
+CONFLUENCE_VERSION=7.10.2
+
 exec atlas-run-standalone \
   --server confluence \
-  --container tomcat7x \
   --product confluence \
-  --version 7.10.2 \
-  --data-version 7.10.2 \
-  --bundled-plugins com.atlassian.bundles:json-schema-validator-atlassian-bundle:1.0.4,com.atlassian.upm:atlassian-universal-plugin-manager-plugin:2.21-D20160128T024330,com.atlassian.jwt:jwt-plugin:1.5.9-0019,com.atlassian.plugins:atlassian-connect-plugin:1.1.77 \
+  --version $CONFLUENCE_VERSION \
+  --data-version $CONFLUENCE_VERSION \
   --jvmargs "${JAVA_OPTS}" \
    -DskipAllPrompts=true
